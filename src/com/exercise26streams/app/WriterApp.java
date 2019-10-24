2bbe001dfed54dfc809a524440233fe0f00d1fe9
PabloSalazar333 <p.e.salazarmarquez@gmail.com>
2019-10-24 12:21:46 -0500

    Example write table math

--- src/com/exercise26streams/app/WriterApp.java
+++ src/com/exercise26streams/app/WriterApp.java
@@ -3,6 +3,7 @@ package com.exercise26streams.app;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 import java.io.BufferedWriter;;
 
 
@@ -11,16 +12,28 @@ public class WriterApp
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		// Constant declaration
+		final int LIMIT = 24;
 		File myFile = null;
 		FileWriter myWriter = null;
 		BufferedWriter myBuffer = null;
+		
+		Scanner input = new Scanner(System.in);
+		int table=0;
+		
 		
 		try {
 			myFile = new File("C:\\a\\b.txt");
 			myWriter = new FileWriter(myFile);
 			myBuffer = new BufferedWriter(myWriter);
-			myBuffer.write("Hola");
+			
+			System.out.print("Input the multiplication table: ");
+			table = input.nextInt();
+			for(int i=1; i<=LIMIT; i++)
+			{
+				System.out.print(String.format("%d X %d = %d \n", table,i,table*i));
+				myBuffer.write(String.format("%d X %d = %d \n", table,i,table*i));//+String.format("%d X %d = %d \n", table,i,table*i));
+			}
 		}
 		catch(IOException e) {
 			e.printStackTrace();
